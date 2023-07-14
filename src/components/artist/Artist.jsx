59be11e59190749963ef952ef063edf2d1283eec
GhazaGG <ghazagg7@gmail.com>
2023-07-14 20:47:09 +0700

    member responsive ( still need to fix )

--- src/components/artist/Artist.jsx
+++ src/components/artist/Artist.jsx
@@ -19,7 +19,7 @@ import ghaza from '../../assets/ghaza/ghaza.png'
 const Artist = () => {
     const navigate= useNavigate()
     const [isHovered, setIsHovered] = useState(false);
-
+    const isMobileView = window.innerWidth <= 767;
 
   return (
     <section className='artist-container'>
@@ -39,7 +39,7 @@ const Artist = () => {
                     <a onClick={()=> navigate('/pageVndttas')} onMouseEnter={() => setIsHovered(true)}
                     onMouseLeave={() => setIsHovered(false)}>
                         <img className='img1' src={vendttas} ></img>
-                        {isHovered && <div className="visit">
+                        {(isHovered || isMobileView) && <div className="visit">
                             <div className='desc-container'>
                                 <p className="nama">VNDTTAS</p>
                                 <p className="role">3D ARTIST</p>
@@ -49,7 +49,7 @@ const Artist = () => {
                     <a onClick={()=> navigate('/pageRyzen')} onMouseEnter={() => setIsHovered(true)}
                     onMouseLeave={() => setIsHovered(false)}>
                         <img className='img2' src={ryzen} ></img>
-                        {isHovered && <div className="visit">
+                        {(isHovered || isMobileView) && <div className="visit">
                             <div className='desc-container'>
                                 <p className="nama">MERYZEN</p>
                                 <p className="role">3D ARTIST</p>
@@ -59,7 +59,7 @@ const Artist = () => {
                     <a onClick={()=> navigate('/pageRio')} onMouseEnter={() => setIsHovered(true)}
                     onMouseLeave={() => setIsHovered(false)}>
                         <img className='img3' src={rio} ></img>
-                        {isHovered && <div className="visit">
+                        {(isHovered || isMobileView) && <div className="visit">
                             <div className='desc-container'>
                                 <p className="nama">SATRIO AJI</p>
                                 <p className="role">3D ARTIST</p>
@@ -69,7 +69,7 @@ const Artist = () => {
                     <a onClick={()=> navigate('/pageBrian')} onMouseEnter={() => setIsHovered(true)}
                     onMouseLeave={() => setIsHovered(false)}>
                         <img className='img4' src={brian} ></img>
-                        {isHovered && <div className="visit">
+                        {(isHovered || isMobileView) && <div className="visit">
                             <div className='desc-container'>
                                 <p className="nama">NIOZZIN</p>
                                 <p className="role">2D ARTIST</p>
@@ -79,7 +79,7 @@ const Artist = () => {
                     <a onClick={()=> navigate('/pageAdam')} onMouseEnter={() => setIsHovered(true)}
                     onMouseLeave={() => setIsHovered(false)}>
                         <img className='img5' src={adam} ></img>
-                        {isHovered && <div className="visit">
+                        {(isHovered || isMobileView) && <div className="visit">
                             <div className='desc-container'>
                                 <p className="nama">ADAMALFYSR</p>
                                 <p className="role">3D ARTIST</p>
@@ -89,7 +89,7 @@ const Artist = () => {
                     <a onClick={()=> navigate('/pageIslan')} onMouseEnter={() => setIsHovered(true)}
                     onMouseLeave={() => setIsHovered(false)}>
                         <img className='img6' src={islan} ></img>
-                        {isHovered && <div className="visit">
+                        {(isHovered || isMobileView) && <div className="visit">
                             <div className='desc-container'>
                                 <p className="nama">ISLANGRH</p>
                                 <p className="role">3D ARTIST</p>
@@ -99,7 +99,7 @@ const Artist = () => {
                     <a onClick={()=> navigate('/pageExhozt')} onMouseEnter={() => setIsHovered(true)}
                     onMouseLeave={() => setIsHovered(false)}>
                         <img className='img7' src={dafa} ></img>
-                        {isHovered && <div className="visit">
+                        {(isHovered || isMobileView) && <div className="visit">
                             <div className='desc-container'>
                                 <p className="nama">EXHOZT</p>
                                 <p className="role">2D ARTIST</p>
@@ -109,7 +109,7 @@ const Artist = () => {
                     <a onClick={()=> navigate('/pageHolikPC')} onMouseEnter={() => setIsHovered(true)}
                     onMouseLeave={() => setIsHovered(false)}>
                         <img className='img8' src={holik} ></img>
-                        {isHovered && <div className="visit">
+                        {(isHovered || isMobileView) && <div className="visit">
                             <div className='desc-container'>
                                 <p className="nama">HOLIKPC</p>
                                 <p className="role">3D ARTIST</p>
@@ -119,7 +119,7 @@ const Artist = () => {
                     <a onClick={()=> navigate('/pageGhaza')} onMouseEnter={() => setIsHovered(true)}
                     onMouseLeave={() => setIsHovered(false)}>
                         <img className='img9' src={ghaza} ></img>
-                        {isHovered && <div className="visit">
+                        {(isHovered || isMobileView) && <div className="visit">
                             <div className='desc-container'>
                                 <p className="nama">GHAZA</p>
                                 <p className="role">IT</p>
@@ -129,7 +129,7 @@ const Artist = () => {
                     <a onClick={()=> navigate('/pageYopan')} onMouseEnter={() => setIsHovered(true)}
                     onMouseLeave={() => setIsHovered(false)}>
                         <img className='img10 ' src={yopan}></img>
-                        {isHovered && <div className="visit">
+                        {(isHovered || isMobileView) && <div className="visit">
                             <div className='desc-container'>
                                 <p className="nama">YOFAN</p>
                                 <p className="role">2D ARTIST</p>
@@ -139,7 +139,7 @@ const Artist = () => {
                     <a onClick={()=> navigate('/pageDoni')} onMouseEnter={() => setIsHovered(true)}
                     onMouseLeave={() => setIsHovered(false)}>
                         <img className='img11 ' src={doni}></img>
-                        {isHovered && <div className="visit">
+                        {(isHovered || isMobileView) && <div className="visit">
                             <div className='desc-container'>
                                 <p className="nama">RAMDHONI</p>
                                 <p className="role">IT</p>
@@ -149,7 +149,7 @@ const Artist = () => {
                     <a onClick={()=> navigate('/pageDepol')} onMouseEnter={() => setIsHovered(true)}
                     onMouseLeave={() => setIsHovered(false)}>
                         <img className='img12 ' src={depol}></img>
-                        {isHovered && <div className="visit">
+                        {(isHovered || isMobileView) && <div className="visit">
                             <div className='desc-container'>
                                 <p className="nama">DEPOAL</p>
                                 <p className="role">GRAPHIC DESIGNER</p>
@@ -160,6 +160,7 @@ const Artist = () => {
             </div>
 
         </div>
+        <footer>&copy; 2023 .12px All Rights Reserved</footer>
     </section>
   )
 }
