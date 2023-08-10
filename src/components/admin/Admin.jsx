b41919e3058ed49bc4fbed24c2cf5f235f53536d
GhazaGG <ghazagg7@gmail.com>
2023-08-10 23:13:45 +0700

    completing logout system

--- src/components/admin/Admin.jsx
+++ src/components/admin/Admin.jsx
@@ -2,12 +2,21 @@ import React, { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../../UserContext'
 import { useNavigate, useParams } from 'react-router-dom'
 import "./admin.css"
+import axios from 'axios'
+
+// tutorial after acount page
 
 const Admin = () => {
-    const {ready,user} = useContext(UserContext)
+    const[redirect,setRedirect] = useState(null)
+    const {ready,user,setUser} = useContext(UserContext)
     const navigate = useNavigate()
     const [isLoading, setIsLoading] = useState(true);
     const [activeView, setActiveView] = useState('member')
+
+    async function logout(){
+        await axios.post('/logout')
+        setRedirect('/')
+    }
 
     useEffect(() => {
         if (ready) {
@@ -19,16 +28,29 @@ const Admin = () => {
         }
     }, [ready, user, navigate]);
 
+    useEffect(() => {
+        if (redirect) {
+            navigate("/");
+            setRedirect(null)
+        }
+    }, [redirect, navigate]);
+
     if (isLoading) {
         return <div>Loading...</div>;
     }
+
+
+    
     
     return (
         <div className='admin-container'>
             <nav className='admin-nav'>
-                <a className={`home ${activeView === 'member' ? 'active' : ''}`} onClick={() => setActiveView('member')}>.12Px Member</a>
-                <a className={`home ${activeView === 'artwork' ? 'active' : ''}`} onClick={() => setActiveView('artwork')}>.12Px Artwork</a>
-                <button className='logout'>Logout</button>
+                <h1 className='user'>Hi {user.username}!</h1>
+                    <div className="navlink">
+                        <a className={`home ${activeView === 'member' ? 'active' : ''}`} onClick={() => setActiveView('member')}>.12Px Member</a>
+                        <a className={`home ${activeView === 'artwork' ? 'active' : ''}`} onClick={() => setActiveView('artwork')}>.12Px Artwork</a>
+                    </div>
+                <button onClick={logout} className='logout'>Logout</button>
             </nav>
 
             <div className="admin-content">
